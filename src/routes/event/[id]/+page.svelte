4bb4ba08a38f0ed57e1adfe863be923d99adda13
t-detlefsen <ted32@pitt.edu>
2026-07-05 17:50:17 -0400

    Update attendee format

--- src/routes/event/[id]/+page.svelte
+++ src/routes/event/[id]/+page.svelte
@@ -395,7 +395,11 @@
 														const day = String(date.getDate()).padStart(2, '0');
 														const hours = String(date.getHours()).padStart(2, '0');
 														const minutes = String(date.getMinutes()).padStart(2, '0');
-														return `${year}/${month}/${day} ${hours}:${minutes}`;
+
+														const date_format = formatDate(`${year}-${month}-${day}`);
+														const time_format = formatTime(`${hours}:${minutes}`);
+
+														return `${date_format} ${time_format}`;
 													})()}
 												</p>
 											</div>
